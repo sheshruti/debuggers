fa660509b42c84e652c5dce3fd6f3ec5f672191d
sheshruti <82359338+sheshruti@users.noreply.github.com>
2021-05-22 10:53:43 +0530

    Delete mernbackend directory

--- mernbackend/src/db/con.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/register",{
-    useNewUrlParser:true, useUnifiedTopology:true,
-    useCreateIndex:true
-}).then(()=>{
-    console.log(`conection is successful`);
-}).catch((e) => {console.log(`no conection`);
-
-})
